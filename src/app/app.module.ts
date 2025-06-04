a60faa0eff26f310473db596454e4fe314bb3255
ViniciusKLima <viniciuskls2006@gmail.com>
2025-06-03 23:52:40 -0300

    add-android

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicModule } from '@ionic/angular';
-import { HttpClientModule } from '@angular/common/http';  // <-- Importar HttpClientModule
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
